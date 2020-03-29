f2e7e8174693254063d21083477e3842e0cf088e
taohuh <pimpakarn@maqe.com>
2020-03-29 14:04:32 +0700

    Setup project

---
+++ script.js
@@ -0,0 +1,26 @@
+const homePage = document.getElementsByClassName("home-page");
+
+function clickBtn(type) {
+	gtag('event', 'click_button', {
+		'event_label': `${type} Button`,
+		'event_category': 'button',
+	});
+}
+
+function clickBtnLike() {
+	clickBtn('Like');
+}
+
+function clickBtnDislike() {
+	clickBtn('Dislike');
+}
+
+if (homePage.length > 0) {
+	document
+		.getElementById("btn-like")
+		.addEventListener("click", clickBtnLike);
+
+	document
+		.getElementById("btn-dislike")
+		.addEventListener("click", clickBtnDislike);
+}
